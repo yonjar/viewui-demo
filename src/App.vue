f8a7be9c4641abbd5d022c725b07e8083791cfd5
yonjar <1332934648@qq.com>
2020-09-27 13:35:56 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+    @import 'styles/common.css';
 </style>
+<template>
+    <div>
+        <router-view></router-view>
+    </div>
+</template>
+<script>
+    export default {
+        data () {
+            return {}
+        },
+        mounted () {
+
+        },
+        beforeDestroy () {
+
+        },
+        methods: {
+
+        }
+    }
+</script>
